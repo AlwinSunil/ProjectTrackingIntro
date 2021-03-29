977250c12860e76eb2539dd38abb1282069bc3d4
AlwinSunil <alv12alwin@gmail.com>
2021-03-29 17:34:55 +0530

    v1.0

---
+++ app/js/script.js
@@ -0,0 +1,35 @@
+function navbarToggle() {
+  document.getElementById("nav-mobile").style.display = "flex";
+  document.getElementById("hamburger").style.display = "none";
+  document.getElementById("close").style.display = "block";
+}
+
+function navbarClose() {
+  document.getElementById("nav-mobile").style.display = "none";
+  document.getElementById("hamburger").style.display = "block";
+  document.getElementById("close").style.display = "none";
+}
+
+const screenWidth = screen.width;
+console.log(screenWidth);
+
+window.onload = function () {
+  warningBanner();
+  window.scrollTo(0, 0);
+};
+
+function warningBanner() {
+  document.getElementById("warning-banner").style.display = "block";
+  if (screenWidth < 750) {
+    setTimeout(() => {
+      document.getElementById("warning-banner").style.height = "10vh";
+    }, 200);
+  } else {
+    setTimeout(() => {
+      document.getElementById("warning-banner").style.height = "7.5vh";
+    }, 200);
+  }
+  setTimeout(() => {
+    document.getElementById("warning-banner-p").style.display = "block";
+  }, 300);
+}
